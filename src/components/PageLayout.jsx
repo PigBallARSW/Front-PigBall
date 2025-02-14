4ab4df98781f2212e44f6e422ae23de88cfce54f
Camila Torres <andrea.torres-g@mail.escuelaing.edu.co>
2025-02-13 23:59:07 -0500

    arreglo login

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -3,7 +3,7 @@
  * Licensed under the MIT License.
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useIsAuthenticated } from '@azure/msal-react';
 import { SignInButton } from './SignInButton';
 import { SignOutButton } from './SignOutButton';
@@ -15,12 +15,21 @@ import { MicrosoftLoginButton } from './MicrosoftLoginButton';
  */
 export const PageLayout = () => {
     //const isAuthenticated = useIsAuthenticated();
-
+    /*useEffect(() => {
+        let p1 = document.getElementById("a");
+        let p2 = document.getElementById("b");
+        let p3 = document.getElementById("c");
+        let p4 = document.getElementById("d");
+        let p5 = document.getElementById("e");
+        let p6 = document.getElementById("f");
+        let ball = document.getElementById("ball");
+        p1.style.top = 30+"px";  
+        p1.style.top = 20+"px";  
+    },[]);*/
     return (
         <div class="container-fluid vh-100 d-flex align-items-center justify-content-center login-container">
             <h1 class="field-title">PIGBALL</h1>
-            <MicrosoftLoginButton />
-            <div class="row w-100 h-100 m-0">
+            <div class="row w-100 h-100 m-0 field">
                 <div class="col-md-6 text-white p-3 first field-login">
                     <div class="court court-left">
                         <div class="court-mini court-mini-left"></div>
@@ -39,6 +48,8 @@ export const PageLayout = () => {
                 </div>
             </div>
             <div class="field-circle"> </div>
+            <div id="ball"> </div>
+            <MicrosoftLoginButton />
         </div>
 
     );
