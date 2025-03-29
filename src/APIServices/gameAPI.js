edeb6680d6d67f14ea58e2c85c7edde11e542974
unknown <mateo.forero-f@labinfo.is.escuelaing.edu.co>
2025-03-29 10:49:52 -0500

    feat: change

--- src/APIServices/gameAPI.js
+++ src/APIServices/gameAPI.js
@@ -2,14 +2,13 @@
 import axios from "axios";
 
 // Leer variables de entorno
-
-
 const API = process.env.REACT_APP_API_URL || process.env.REACT_APP_API_URL_LOCAL;
 
 export async function createRoom(newRoom, playerName) {
     try {
         const creatorName = playerName || "default";
         const endpoint = `${API}/lobby`;
+        console.log(endpoint);
         const requestBody = {
             lobbyName: newRoom.name,
             creatorName: creatorName,
